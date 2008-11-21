59e87d22327f4ed808fcf40cbc8fc0761f5ebca9
Philip <philipjagielski@gmail.com>
2008-11-21 01:18:30 -0600

    read a song file complete

--- read_song.h
+++ read_song.h
@@ -1,8 +1,39 @@
 #pragma once
 
+#include "../../std_lib_facilities.h"
+
 /** This file contains a function that reads in a song file into a vector
- * of ints.
+ * of ints. It returns an empty vector of an error occurs.
  */
 vector<int> read_song_file( string filename )
 {
+	vector<int> notes;
+	ifstream indata;
+	indata.open( filename.c_str() );
+	if( !indata )
+	{
+		cerr << "Error: file could not be opened" << endl;
+		//return empty vector if the file can't be opened
+		return notes;
+	}
+	
+	int num = 0;
+	while( !indata.eof() )
+	{
+		indata >> num;
+		notes.push_back( num );
+	}
+	indata.close();
+	return notes;
+}
+
+//for testing
+int main()
+{
+	vector<int> song = read_song_file( "test.txt" );
+
+	//test loop to print contents of song
+	for( int i = 0; i < song.size(); i++ )
+		cout << song[i] << " ";
+	keep_window_open();
 }
